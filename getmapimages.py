0166ad965b408116a2cf74b7306821696336b5e0
jcluber2 <jc.luber2@gmail.com>
2014-12-14 12:07:42 -0600

    Create getmapimages.py

---
+++ getmapimages.py
@@ -0,0 +1,32 @@
+import csv
+import urllib
+import os
+from os.path import isfile, join
+
+UScitylist = {}
+
+with open('GL2_city_loc.csv','rb') as csvfile:
+    reader = csv.reader(csvfile,delimiter='\t')
+    for row in reader:
+        if (row[3]=="US"):
+            if row[6] in UScitylist.keys():
+                UScitylist[row[6]].append(row[7])
+            else:
+                UScitylist[row[6]] = []
+                UScitylist[row[6]].append(row[7])
+
+#print UScitylist['Illinois']
+
+roadmapurl = "https://maps.googleapis.com/maps/api/staticmap?zoom=18&size=800x800&maptype=roadmap"
+satelliteurl = "https://maps.googleapis.com/maps/api/staticmap?zoom=18&size=800x800&maptype=satellite"
+
+#for city in UScitylist['Illinois']:
+#    urllib.urlretrieve(roadmapurl+"&center="+city, "mapdata/"+city+"_Illinois_roadmap.jpg")
+#    urllib.urlretrieve(roadmapurl+"&center="+city, "mapdata/"+city+"_Illinois_satellite.jpg")
+ 
+onlyfiles = [ f for f in os.listdir("mapdata/") if isfile(join("mapdata/",f)) ]
+
+for i in onlyfiles:
+    city = i.split('_')
+    #print satelliteurl+"&center="+city[0].lower()
+    urllib.urlretrieve(satelliteurl+"&center="+city[0].lower(), "mapdata/"+city[0]+"_Illinois_satellite.jpg")
